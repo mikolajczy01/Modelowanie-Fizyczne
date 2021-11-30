104dd783e43a805163025e937acedd2148f30d63
mikolajczy01 <47851742+mikolajczy01@users.noreply.github.com>
2021-11-30 13:38:46 +0100

    Streamplot

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
+import sympy as sp
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib
 matplotlib.use('TkAgg')
-
 
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -51,7 +51,7 @@ LiniePola(wykresy,x,y)
 
 
 wykresy = fig2.add_subplot(1,1,1)
-y=x**2
+y=x**2+x
 LiniePola(wykresy,x,y)
 
 
@@ -59,10 +59,26 @@ wykresy = fig3.add_subplot(1,1,1)
 y=(x**3)+float(wsp[0])
 LiniePola(wykresy,x,y)
 
+Y, X = np.mgrid[0:3,0:3]
+U = X+1
+V = Y**2
+
+print(X)
+print(Y)
+print(U)
+print(V)
+
+fig4 = plt.figure(figsize=(12, 7), dpi=200)
+
+ax0 = fig4.add_subplot(1,1,1)
+strm = ax0.streamplot(X, Y, U, V, density=2,color=V,cmap='winter',linewidth=1)
+fig4.colorbar(strm.lines)
+ax0.set_title('Varying Density')
+ax0.grid(True)
 
 #okno wykresiory
 
-lista_wykresow = ["essa","lessa","julkakulka"]
+lista_wykresow = ["essa","lessa","julkakulka","Linie prądu"]
 
 wybor_wykresu = [
     [sg.Text("Dostępne wykresy: ")],
@@ -88,9 +104,9 @@ layout = [
 
 window = sg.Window('Essa gaming', layout, finalize=True, element_justification='center',font='Helvetica 18',location=(0, 0),resizable=True)
 
-window["-WYKRESY-"].update(set_to_index=0)
+window["-WYKRESY-"].update(set_to_index=3)
 
-fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig4)
 
 while True:
 
@@ -109,5 +125,7 @@ while True:
             fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig2)
         elif(values['-WYKRESY-'][0]=="julkakulka"):
             fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig3)
+        elif(values['-WYKRESY-'][0]=="Linie prądu"):
+            fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig4)
 
 window.close()
