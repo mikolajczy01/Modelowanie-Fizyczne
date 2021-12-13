168b182fb57a666e1e54af9a55db8047328d6a3a
mikolajczy01 <47851742+mikolajczy01@users.noreply.github.com>
2021-12-14 00:06:26 +0100

    nie wiadomo co sie tu dzieje

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib
 matplotlib.use('TkAgg')
 
+sp.init_printing()
+x,y = sp.symbols('x y')
+h = sp.Symbol('h')
+k = sp.Symbol('k')
+def rozniczka_x(f):
+    return (f.diff(x))
+
+def rozniczka_y(f):
+    return (f.diff(y))
+
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
@@ -17,17 +27,17 @@ def delete_fig_agg(fig_agg):
     fig_agg.get_tk_widget().forget()
     fig_agg.get_tk_widget().delete('all')
 
-def LiniePola(wykres,x,y):
+def LiniePola(wykres,x1,y1):
     for c in range(-100,100,1):
-        wykres.plot(x,y+c, c=(0.5, 0.5, 0.5, 0.5))
+        wykres.plot(x1,y1+c, c=(0.5, 0.5, 0.5, 0.5))
     wykres.grid(True)
     wykres.set_xlim(0,20)
     wykres.set_ylim(0,20)
 
 #okno pobieranie
 layout = [[sg.Text("Wykres wariacie")],
-            [sg.Text("Ux ="),sg.Input()],
-            [sg.Text("Uy ="),sg.Input()],
+            [sg.Text("Ux ="),sg.InputText()],
+            [sg.Text("Uy ="),sg.InputText()],
             [sg.Button('Dalej')]]
 
 window = sg.Window('Essa gaming', layout, finalize=True, element_justification='center',font='Helvetica 18')
@@ -35,43 +45,48 @@ window = sg.Window('Essa gaming', layout, finalize=True, element_justification='
 event, wsp = window.read()
 
 window.close()
-
 #WYKRESIORY
 
 fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
 fig2 = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
 fig3 = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
 
-x = np.arange(0,1000)
+x1 = np.arange(0,1000)
 
 wykresy = fig.add_subplot(1,1,1)
-y=x*float(wsp[0])+float(wsp[1])
-LiniePola(wykresy,x,y)
+y1=x1
+LiniePola(wykresy,x1,y1)
 
 
 
 wykresy = fig2.add_subplot(1,1,1)
-y=x**2+x
-LiniePola(wykresy,x,y)
+y1=x1**2+x1
+LiniePola(wykresy,x1,y1)
 
 
 wykresy = fig3.add_subplot(1,1,1)
-y=(x**3)+float(wsp[0])
-LiniePola(wykresy,x,y)
+y1=(x1**3)
+LiniePola(wykresy,x1,y1)
 
-Y, X = np.mgrid[0:3,0:3]
-U = X+1
-V = Y**2
+Y,X = np.mgrid[0:20:100j,0:20:100j]
+V,U = np.mgrid[0:20:100j,0:20:100j]
 
-print(X)
-print(Y)
-print(U)
-print(V)
+f = sp.integrate(sp.sympify(wsp[0]),x)
+g = sp.integrate(sp.sympify(wsp[1]),y)
+sp.pprint(g, use_unicode=True)
+sp.pprint(f, use_unicode=True)
+
+for i in range(3):
+    for j in range(3):
+        x1 = X[i,j]
+        y1 = Y[i,j]
+        U[i,j] = f.subs({x:x1, y:y1})
+        V[i,j] = g.subs({x:x1, y:y1})
 
 fig4 = plt.figure(figsize=(12, 7), dpi=200)
 
 ax0 = fig4.add_subplot(1,1,1)
-strm = ax0.streamplot(X, Y, U, V, density=2,color=V,cmap='winter',linewidth=1)
+strm = ax0.streamplot(X, Y, U, V, density=1,color=V,cmap='winter',linewidth=1)
 fig4.colorbar(strm.lines)
 ax0.set_title('Varying Density')
 ax0.grid(True)
