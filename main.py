852e58a89c24856770b3c6c0837a7bcbe32f2938
mikolajczy01 <47851742+mikolajczy01@users.noreply.github.com>
2021-12-14 02:42:10 +0100

    COCODZAMBO I DO PRZODU

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ matplotlib.use('TkAgg')
 
 sp.init_printing()
 x,y = sp.symbols('x y')
-h = sp.Symbol('h')
-k = sp.Symbol('k')
-def rozniczka_x(f):
-    return (f.diff(x))
-
-def rozniczka_y(f):
-    return (f.diff(y))
 
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -38,6 +31,8 @@ def LiniePola(wykres,x1,y1):
 layout = [[sg.Text("Wykres wariacie")],
             [sg.Text("Ux ="),sg.InputText()],
             [sg.Text("Uy ="),sg.InputText()],
+            [sg.Text("Xo ="),sg.InputText()],
+            [sg.Text("Yo ="),sg.InputText()],
             [sg.Button('Dalej')]]
 
 window = sg.Window('Essa gaming', layout, finalize=True, element_justification='center',font='Helvetica 18')
@@ -47,61 +42,65 @@ event, wsp = window.read()
 window.close()
 #WYKRESIORY
 
-fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
-fig2 = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
-fig3 = matplotlib.figure.Figure(figsize=(5, 4), dpi=200)
+#wykres linii prądu
+Y,X = np.mgrid[1:20:100j,1:20:100j]
+V,U = np.mgrid[1:20:100j,1:20:100j]
 
-x1 = np.arange(0,1000)
+f = sp.integrate(1/sp.sympify(wsp[0]),x)
+g = sp.integrate(1/sp.sympify(wsp[1]),y)
+#
+wynik = sp.sympify(sp.solve(sp.Eq(f,g),y))
 
-wykresy = fig.add_subplot(1,1,1)
-y1=x1
-LiniePola(wykresy,x1,y1)
+sp.pprint(f, use_unicode=True)
+sp.pprint(g, use_unicode=True)
+sp.pprint(wynik[0], use_unicode=True)
 
+for i in range(100):
+    for j in range(100):
+        x1 = X[i,j]
+        y1 = Y[i,j]
+        V[i, j] = wynik[0].subs({x: x1, y: y1})
 
+fig1 = plt.figure(figsize=(12, 7), dpi=200)
 
-wykresy = fig2.add_subplot(1,1,1)
-y1=x1**2+x1
-LiniePola(wykresy,x1,y1)
+ax0 = fig1.add_subplot(1,1,1)
+strm = ax0.streamplot(X, Y, X, V, density=1,color=V,cmap='winter',linewidth=1)
+fig1.colorbar(strm.lines)
+ax0.set_title('Varying Density')
+ax0.grid(True)
 
+#wykres toru
 
-wykresy = fig3.add_subplot(1,1,1)
-y1=(x1**3)
-LiniePola(wykresy,x1,y1)
-
-Y,X = np.mgrid[0:20:100j,0:20:100j]
-V,U = np.mgrid[0:20:100j,0:20:100j]
+Y,X = np.mgrid[1:20,1:20]
+V,U = np.mgrid[1:20,1:20]
 
 f = sp.integrate(sp.sympify(wsp[0]),x)
 g = sp.integrate(sp.sympify(wsp[1]),y)
-sp.pprint(g, use_unicode=True)
-sp.pprint(f, use_unicode=True)
+# sp.pprint(g, use_unicode=True)
+# sp.pprint(f, use_unicode=True)
 
-for i in range(3):
-    for j in range(3):
+for i in range(19):
+    for j in range(19):
         x1 = X[i,j]
         y1 = Y[i,j]
-        U[i,j] = f.subs({x:x1, y:y1})
-        V[i,j] = g.subs({x:x1, y:y1})
+        U[i,j] = sp.sympify(wsp[0]).subs({x:x1, y:y1})
+        V[i,j] = sp.sympify(wsp[1]).subs({x:x1, y:y1})
 
-fig4 = plt.figure(figsize=(12, 7), dpi=200)
+fig2 = plt.figure(figsize=(12, 7), dpi=200)
 
-ax0 = fig4.add_subplot(1,1,1)
+ax0 = fig2.add_subplot(1,1,1)
 strm = ax0.streamplot(X, Y, U, V, density=1,color=V,cmap='winter',linewidth=1)
-fig4.colorbar(strm.lines)
+fig2.colorbar(strm.lines)
 ax0.set_title('Varying Density')
 ax0.grid(True)
 
 #okno wykresiory
 
-lista_wykresow = ["essa","lessa","julkakulka","Linie prądu"]
+lista_wykresow = ["Linie prądu","Tor elementu płynu"]
 
 wybor_wykresu = [
     [sg.Text("Dostępne wykresy: ")],
-    [
-        sg.Listbox(
-            values=lista_wykresow, enable_events=True, size=(40, 20), key="-WYKRESY-",
-        )
-    ]
+    [sg.Listbox(values=lista_wykresow, enable_events=True, size=(40, 20), key="-WYKRESY-")]
 ]
 
 wykres = [
@@ -119,9 +118,9 @@ layout = [
 
 window = sg.Window('Essa gaming', layout, finalize=True, element_justification='center',font='Helvetica 18',location=(0, 0),resizable=True)
 
-window["-WYKRESY-"].update(set_to_index=3)
+window["-WYKRESY-"].update(set_to_index=0)
 
-fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig4)
+fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig1)
 
 while True:
 
@@ -134,13 +133,8 @@ while True:
         if fig_agg is not None:
             delete_fig_agg(fig_agg)
             window.Refresh()
-        if values['-WYKRESY-'][0]=="essa":
-            fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
-        elif(values['-WYKRESY-'][0]=="lessa"):
+        if values['-WYKRESY-'][0]=="Linie prądu":
+            fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig1)
+        elif(values['-WYKRESY-'][0]=="Tor elementu płynu"):
             fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig2)
-        elif(values['-WYKRESY-'][0]=="julkakulka"):
-            fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig3)
-        elif(values['-WYKRESY-'][0]=="Linie prądu"):
-            fig_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig4)
-
 window.close()
