d06b17e8ae9edf13df88ab0df6aedf3cd94b4839
mikolajczy01 <47851742+mikolajczy01@users.noreply.github.com>
2022-01-03 20:50:43 +0100

    Nie moge juz wytrzymiec

--- main.py
+++ main.py
@@ -21,16 +21,9 @@ def bar(n,i,j,k):
         [sg.Cancel()]
     ]
     window = sg.Window('Pole prędkości', layout,no_titlebar=True, alpha_channel=1, grab_anywhere=True)
-    progress_bar = window['progressbar']
-    while True:
-        event, values = window.read(timeout=10)
-        if event == 'Cancel' or event is None:
-            break
-        else:
-            for i in range(0, n):
-                progress_bar.UpdateBar(i + j+k)
-        window.close()
-    return progress_bar
+    #progress_bar = window['progressbar']
+    return window
+
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
@@ -72,6 +65,7 @@ window = sg.Window('Pole Prędkości', layout, finalize=True, element_justificat
 event, wsp = window.read()
 
 window.close()
+
 #WYKRESIORY
 
 
@@ -89,11 +83,14 @@ sp.pprint(g, use_unicode=True)
 sp.pprint(wynik[0], use_unicode=True)
 
 progressbar=bar(10000,100,100,0)
+
 for i in range(100):
     for j in range(100):
         V[i, j] = wynik[0].subs({x: X[i, j]})
         diver[i, j] = sp.sympify(div(wsp[0], wsp[1])).subs({x: X[i, j], y: Y[i, j]})
-        progressbar.UpdateBar(i * 100 + j)
+        progressbar.Read(timeout=0)
+        progressbar['progressbar'].UpdateBar(i*100+j)
+progressbar.close()
 
 fig1 = plt.figure(figsize=(12, 7), dpi=200)
 
@@ -112,19 +109,17 @@ f = sp.integrate(1/sp.sympify(wsp[0]),x)
 g = sp.integrate(1/sp.sympify(wsp[1]),y)
 wynik = sp.sympify(sp.solve(sp.Eq(f,g),y))
 
-progressbar(1000,100,0)
+progressbar=bar(1000,100,0,0)
 for i in range(100):
         Y[i]=wynik[0].subs({x: X[i]})
-        progressbar.UpdateBar(i)
+        progressbar.Read(timeout=0)
+        progressbar['progressbar'].UpdateBar(i)
+progressbar.close()
+
 fig2 = plt.figure(figsize=(12, 7), dpi=200)
 
 ax0 = fig2.add_subplot(1,1,1)
 strm = ax0.plot(X, Y)
-# print(X[50])
-# print(Y[50])
-# print(X[51])
-# print(Y[51])
-# ax0.arrow(X[50],Y[50],X[51]-X[50],Y[51]-Y[50], shape='full', lw=0, length_includes_head=True)
 ax0.set_title('Tor elementu płynu')
 ax0.grid(True)
 
@@ -143,7 +138,7 @@ sp.pprint(wynikx, use_unicode=True)
 sp.pprint(wyniky, use_unicode=True)
 sp.pprint(wynikz, use_unicode=True)
 
-progressbar(8000,20,20,20)
+progressbar=bar(8000,20,20,20)
 
 for i in range(19):
     for j in range(19):
@@ -151,10 +146,17 @@ for i in range(19):
             U[i, j, k]=wynikx.subs({z: Z[i, j], y: Y[i, j],x: X[i,j]})
             V[i, j, k]=wyniky.subs({z: Z[i, j], y: Y[i, j],x: X[i,j]})
             W[i, j, k]=wynikz.subs({z: Z[i, j], y: Y[i, j],x: X[i,j]})
-            progressbar.UpdateBar(i*20+j*20+k)
-strm=ax0.quiver(X,Y,Z,U,V,W, length=0.1)
+            progressbar.Read(timeout=0)
+            progressbar['progressbar'].UpdateBar(i * 100 + j)
+
+progressbar.close()
+
+skip=(slice(None,None,3),slice(None,None,3),slice(None,None,3))
+strm=ax0.quiver(X[skip],Y[skip],Z[skip],U[skip],V[skip],W[skip])
 ax0.set_title('Rotacja')
 ax0.grid(True)
+
+
 
 #okno wykresiory
 sg.theme('DarkBlue2')
